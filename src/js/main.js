493ff2ed85ea290e00794d8b03325e6af3579f29
Tania Salvatella <tsalvatellap@gmail.com>
2025-01-23 17:10:23 +0100

    btn-collapse

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 'use strict';
-import data from "./data.json";
-console.log (data);
+// import data from "./data.json";
+// console.log (data);
 
+const closeBtn = document.querySelector(".js-closeBtn");
+const menu = document.querySelector(".js-menu");
+const menuOpen = document.querySelector(".js-menuOpen");
+
+menuOpen.addEventListener("click", () => {
+  menu.classList.remove("collapsed"); // Abre el menú
+  console.log("abre menu");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.add("collapsed"); // Cierra el menú
+  console.log("cierra menu");
+});
