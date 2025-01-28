f6d90854fa11b730eb3c6d9b15d33caccdea8f06
Tania Salvatella <tsalvatellap@gmail.com>
2025-01-28 08:20:41 +0100

    project video fixed

--- src/js/0_projects.js
+++ src/js/0_projects.js
@@ -1,4 +1,5 @@
 import data from "./projects.json";
+
 /* 
 1- En el menu de projectos al clickar en una imagen, tiene que abrir la página de ese proyecto. (un evento llama al id concreto del proyecto) click === data.id
 2- Se abre la página nueva de un projecto. Ahi es donde se tiene que pintar 
@@ -7,39 +8,59 @@ Otra función para recorrer los arrays y pintar todos los objetos
 ejecutar al cargar la página la función que recorre el array
 
 */
-const gallery = document.querySelector ('.js-gallery');
 
-//función para recorrer el array y que devuelva todos lo projectos. Esta función hay que llamarla en la página projects.html para no tener que ir añadiendo projectos nuevos, que vaya creciendo la lista
+const gallery = document.querySelector('.js-gallery');
 
-function renderGallery (){
+// Función para recorrer el array y renderizar la galería
+function renderGallery() {
   const sortedData = [...data].sort((a, b) => b.id - a.id);
-  let mediaElement = "";
-    for (const project of sortedData){
-      if (project.type === "video") {
-        const thumbnailUrl = project.thumbnail || ""; 
-        mediaElement = `<img class="project_img" src="${thumbnailUrl}" alt="${project.title}" data-id="${project.id}">`; //Guardamos la url del video
-    } else if (project.type === "image") {
-        mediaElement = `<img class="project_img" src="${project.images[0]}" alt="${project.title}" data-id="${project.id}">`;
-    } 
-    gallery.innerHTML += 
-    `<a href="./template-each-project.html?id=${project.id}" class="project-link">
-        ${mediaElement}
-        <p>${project.title}</p>
-     </a>`;
-    }
-    // Seleccionar todas las imagenes,  recorrerlas y escuchar el ev sobre cada una de las  imagenes
-    const allImages = document.querySelectorAll  ('.js-images');  
-    for (const oneImage of allImages){
-      oneImage.addEventListener ('click', handleClickImg);
+  gallery.innerHTML = ""; // Limpiar la galería antes de renderizar
 
+  for (const item of sortedData) {
+    const imageDescription = item.imageDescriptions[0] || "Descripción no disponible";
+    const mediaSrc = item.images[0] || "";
+    let mediaElement = "";
+
+    // Renderizar según el tipo de archivo
+    if (mediaSrc.includes("youtube.com") || mediaSrc.includes("vimeo.com")) {
+      mediaElement = `
+        <iframe class="project_img"
+          src="${mediaSrc}" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+        </iframe>`;
+    } else if (/\.(mp4|webm|ogg)$/i.test(mediaSrc)) {
+      mediaElement = `
+        <video class="project_img controls>
+          <source src="${mediaSrc}" type="video/${mediaSrc.split('.').pop()}">
+        </video>`;
+    } else {
+      mediaElement = `<img class="project_img js-images" src="${mediaSrc}" alt="${imageDescription}" data-id="${item.id}"/>`;
     }
-  
+
+    // Añadir el proyecto a la galería
+    gallery.innerHTML += `
+      <a href="./template-each-project.html?id=${item.id}" class="project-link">
+        ${mediaElement}
+        <p>${item.title}</p>
+      </a>`;
   }
 
-renderGallery ();
+  // Agregar eventos a las imágenes
+  addImageClickEvents();
+}
 
-//Funcion manejadora, al hacer click en la imagen, se abre la página del proyecto. ESto lo he copiado, no sé si funciona.
-function handleClickImg (event) {
+// Función para añadir eventos a las imágenes
+function addImageClickEvents() {
+  const allImages = document.querySelectorAll('.js-images');
+  for (const oneImage of allImages) {
+    oneImage.addEventListener('click', handleClickImg);
+  }
+}
+
+// Manejador de eventos al hacer clic en una imagen
+function handleClickImg(event) {
   const clickedImg = event.target;
   const projectId = clickedImg.dataset.id; // Obtener el ID del proyecto
   if (projectId) {
@@ -47,3 +68,5 @@ function handleClickImg (event) {
   }
 }
 
+// Ejecutar la función para renderizar la galería al cargar la página
+renderGallery();
