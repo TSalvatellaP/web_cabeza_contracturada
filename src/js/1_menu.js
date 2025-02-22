9812824dbf1c2a28589792773e368efe89eba0ac
Tania Salvatella <tsalvatellap@gmail.com>
2025-02-22 11:47:03 +0100

    transform image projects

--- src/js/1_menu.js
+++ src/js/1_menu.js
@@ -1,15 +1,9 @@
 'use strict';
-
-const closeBtn = document.querySelector(".js-closeBtn");
 const menu = document.querySelector(".js-menu");
 const menuOpen = document.querySelector(".js-menuOpen");
 
 menuOpen.addEventListener("click", () => {
-  menu.classList.remove("collapsed"); // Abre el menú
+  menu.classList.toggle("collapsed"); // Abre el menú
   console.log("abre menu");
 });
 
-closeBtn.addEventListener("click", () => {
-  menu.classList.add("collapsed"); // Cierra el menú
-  console.log("cierra menu");
-});
