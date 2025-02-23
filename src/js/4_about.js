bffa519a066eebc72087f5382f8c355896bb3210
Tania Salvatella <tsalvatellap@gmail.com>
2025-02-23 09:14:30 +0100

    test foto 3

--- src/js/4_about.js
+++ src/js/4_about.js
@@ -4,7 +4,7 @@ const modalImg = document.getElementById('imgModal');
 const captionText = document.getElementById('caption');
 const closeBtn = document.querySelector('.js-close');
 const specialImage = document.querySelector('.js-img_special');
-const imageForSpecial = "/images/me/me_03.jpg";
+const imageForSpecial = ("/images/me/me_03.jpg");
 
 // Abre el modal con la imagen ampliada
 images.forEach(image => {
